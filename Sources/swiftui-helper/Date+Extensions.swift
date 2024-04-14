6d78cff8ee19044a16167045d4ef0524baa06944
Mushrankhan <mushrankhan76@gmail.com>
2024-04-14 13:19:18 +0530

    Update Date+Extensions.swift

--- Sources/swiftui-helper/Date+Extensions.swift
+++ Sources/swiftui-helper/Date+Extensions.swift
@@ -1,35 +1,35 @@
 import Foundation
 
-extension Date {
-    func month() -> Int {
+public extension Date {
+    public func month() -> Int {
         let calendar = Calendar.current
         return calendar.component(.month, from: self)
     }
     
-    func year() -> Int {
+    public func year() -> Int {
         let calendar = Calendar.current
         return calendar.component(.year, from: self)
     }
     
-    func shortString() -> String {
+    public func shortString() -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "MM/yyyy"
         return formatter.string(from: self)
     }
     
-    func mediumString() -> String {
+    public func mediumString() -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "MMMM dd, yyyy"
         return formatter.string(from: self)
     }
     
-    func longString() -> String {
+    public func longString() -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "EEEE, MMMM dd, yyyy"
         return formatter.string(from: self)
     }
     
-    func monthAndYear() -> String {
+    public func monthAndYear() -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "MMMM-yyyy"
         return formatter.string(from: self)
